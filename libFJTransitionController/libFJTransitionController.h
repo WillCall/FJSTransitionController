114a3f52157a7c252ba2596e2f33ee93e51fc8f4
Patrick Tescher <pat2man@gmail.com>
2011-10-24 12:17:42 -0700

    Added support for building as a static library.

---
+++ libFJTransitionController/libFJTransitionController.h
@@ -0,0 +1,13 @@
+//
+//  libFJTransitionController.h
+//  libFJTransitionController
+//
+//  Created by Patrick Tescher on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libFJTransitionController : NSObject
+
+@end
---
+++ libFJTransitionController/libFJTransitionController.m
@@ -0,0 +1,23 @@
+//
+//  libFJTransitionController.m
+//  libFJTransitionController
+//
+//  Created by Patrick Tescher on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "libFJTransitionController.h"
+
+@implementation libFJTransitionController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
